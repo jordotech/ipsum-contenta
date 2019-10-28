10f337f1f4a83add5b1acb509caa33a3050467a7
Jordan <Austin>
2019-10-28 13:13:18 -0500

    do not ignore entire www folder, only subfolders core, vendor etc. new php DockerfileBase saves build time

--- docker/php/build_and_push.sh
+++ docker/php/build_and_push.sh
@@ -4,4 +4,5 @@ if [ -n "$1" ]; then
 else
     TAG='latest'
 fi
+docker build --rm=false -f DockerfileBase -t jordotech/ipsum_php_base:$TAG . && docker push jordotech/ipsum_php:$TAG
 docker build --rm=false -t jordotech/ipsum_php:$TAG . && docker push jordotech/ipsum_php:$TAG
